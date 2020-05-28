cac77b85dccac7dd8dbb5d632ad5088f6b5406fa
Rachel S. Lee <rslee0025@gmail.com>
2020-05-28 01:09:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,10 @@
 def countdown(num)
-  
-  num = 10 
-  
-  while num > 0
-    puts "#{num} SECOND(S)!"
-    num -= 1
+
+while num > 0
+
+  puts "#{num} SECOND(S)!"
+  num -= 1
+
 end
-  puts "HAPPY NEW YEAR!"
-end  
-  
+  "HAPPY NEW YEAR!"
+end
