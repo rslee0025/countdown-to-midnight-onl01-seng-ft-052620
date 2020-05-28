4c7868722d9f8507c5b4cccc143ad4fc6a96ee1c
Rachel S. Lee <rslee0025@gmail.com>
2020-05-28 01:11:49 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,17 @@ while num > 0
 end
   "HAPPY NEW YEAR!"
 end
+
+
+
+def countdown_with_sleep(num)
+
+while num > 0
+
+  puts "#{num} SECOND(S)!"
+  sleep(1)
+  num -= 1
+
+end
+  "HAPPY NEW YEAR!"
+end	end
