c0dfa13f91399147fdd416bae1fd50db69001411
Rachel S. Lee <rslee0025@gmail.com>
2020-05-28 01:02:25 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
-#write your code here
-
 def countdown
+  counter = 10 
+    while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
 end
+  puts "HAPPY NEW YEAR!"
+  
+  def countdown_with_sleep
+    time = Time.now
+    sleep 2.seconds until Time.now > time + 10.seconds
+  end  
